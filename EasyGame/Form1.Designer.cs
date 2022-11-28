734afda7117d07f339b68b2c2ea7a1d7277abf28
RAZVAN TRUFIN <razvantrufin99x@gmail.com>
2022-11-28 03:16:14 +0200

    Add project files.

---
+++ EasyGame/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace EasyGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PLAYER = new EasyGame.UserControl1();
+            this.userControl11 = new EasyGame.UserControl1();
+            this.SuspendLayout();
+            // 
+            // PLAYER
+            // 
+            this.PLAYER.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.PLAYER.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PLAYER.Location = new System.Drawing.Point(45, 45);
+            this.PLAYER.Name = "PLAYER";
+            this.PLAYER.Size = new System.Drawing.Size(40, 40);
+            this.PLAYER.TabIndex = 0;
+            this.PLAYER.TabStop = false;
+            // 
+            // userControl11
+            // 
+            this.userControl11.BackColor = System.Drawing.Color.Black;
+            this.userControl11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.userControl11.Enabled = false;
+            this.userControl11.Location = new System.Drawing.Point(4, 4);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(604, 606);
+            this.userControl11.TabIndex = 1;
+            this.userControl11.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 612);
+            this.Controls.Add(this.PLAYER);
+            this.Controls.Add(this.userControl11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private UserControl1 PLAYER;
+        private UserControl1 userControl11;
+    }
+}
+
---
+++ EasyGame/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EasyGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Text = e.KeyChar.ToString();
+
+            if (e.KeyChar == 'W' || e.KeyChar == 'w' )
+            {
+                if (PLAYER.Top > 40)
+                {
+                    PLAYER.Top -= 40;
+                }
+            }
+            else if (e.KeyChar == 'A' || e.KeyChar == 'a')
+            {
+                if (PLAYER.Left > 40)
+                {
+                    PLAYER.Left -= 40;
+                }
+            }
+            else if (e.KeyChar == 'D' || e.KeyChar == 'd')
+            {
+                if (PLAYER.Left < 560)
+                {
+                    PLAYER.Left += 40;
+                }
+            }
+            else if (e.KeyChar == 'S' || e.KeyChar == 's')
+            {
+                if (PLAYER.Top < 560)
+                {
+                    PLAYER.Top += 40;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Y < PLAYER.Top)
+            {
+                if (PLAYER.Top > 40)
+                {
+                    PLAYER.Top -= 40;
+                }
+            }
+            else if (e.X < PLAYER.Left)
+            {
+                if (PLAYER.Left > 40)
+                {
+                    PLAYER.Left -= 40;
+                }
+            }
+            else if (e.X > PLAYER.Left+41)
+            {
+                if (PLAYER.Left < 560)
+                {
+                    PLAYER.Left += 40;
+                }
+            }
+            else if (e.Y > PLAYER.Top+41)
+            {
+                Text = "debug";
+                if (PLAYER.Top < 560)
+                {
+                    PLAYER.Top += 40;
+                }
+            }
+        }
+    }
+}
